4ea391b98181dc13e62e6eed5508082e9e260bf1
ndmc402010104 <ndmc402010104@gmail.com>
2026-07-23 23:48:47 +0800

    Bump version for layout metrics capability

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 /*
 檔案位置：jonaminz/version.js
-時間戳：2026-07-17 00:01 UTC+8
+時間戳：2026-07-23 23:46 UTC+8
 用途：jonaminz 業務版本宣告。
 獨立於 SKHPS，這不是 SKHPS_APP_VERSION。這個檔案的 version 字串本身
 2026-07-12 起也兼任全站資源的 cache-buster（見 assets/js/entry-core.js
@@ -12,12 +12,12 @@
 
   window.JONAMINZ_APP_VERSION = {
     appId: "jonaminz",
-    version: "v0.46.43-202607170001",
+    version: "v0.47.0-202607232346",
     major: 0,
-    minor: 46,
-    patch: 43,
-    buildTime: "202607170001",
-    updatedAt: "2026-07-17T00:01:00+08:00",
+    minor: 47,
+    patch: 0,
+    buildTime: "202607232346",
+    updatedAt: "2026-07-23T23:46:00+08:00",
     source: "version.js"
   };
 })();
